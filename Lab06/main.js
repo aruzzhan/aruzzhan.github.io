733042e3ed034b46f483e45a0e87f0a193aadfbc
aruzzhan <you@example.com>
2023-02-28 23:07:00 +0600

    Lab06 commit

---
+++ Lab06/main.js
@@ -0,0 +1,31 @@
+
+function perimeterrectangle (a,b) {
+        result=a*2+b*2;
+        return result
+
+}
+console.log (perimeterrectangle (3,4))
+
+function equality (x,y) {
+    if (x==y) {
+        console.log ("x and y are equal")
+    }
+    else {
+        console.log ("x and y are not equal")
+    }
+}
+console.log (equality (5,8))
+
+function greatest(m,n) {
+    if (m>n) {
+        console.log ("m is the greatest number")
+    }
+    else if (m<n) {
+        console.log ("n is the greatest number")
+    }
+    else {
+        console.log ("m and n are equal numbers")
+    }
+}
+console.log (greatest (6,11))
+
