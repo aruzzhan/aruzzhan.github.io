44fb379ef6f933175f1b8720d71828bf574d5da2
aruzzhan <you@example.com>
2023-04-20 20:02:10 +0600

    Lab

--- Lab09/script.js
+++ Lab09/script.js
@@ -43,61 +43,50 @@ function load() {
   
     svg.append('g')
       .call(yAxis);
-    
-  d3.csv('titanic.csv').then (function (data) {
-    console.log ("Age category:" + parseInt(data [1].Age/10));
-    var grouped=d3.group (data, d=> parseInt(d.Age/10));
-    console.log (grouped);
-  })
-  
-d3.csv("titanic.csv").then(function(data) {
-  var ageGrouped = d3.group(data, d => Math.floor(d.Age/10));
 
-  var ageCounts = Array.from(ageGrouped, ([age, group]) => ({ age: age, count: group.length }));
-
-  var width = 500;
-  var height = 500;
-  var radius = Math.min(width, height) / 2;
-
-
-  var colorScale = d3.scaleOrdinal()
-    .domain(ageCounts.map(d => d.age))
-    .range(d3.schemeCategory20);
-
-  var pie = d3.pie()
-    .value(d => d.count)
-    .sort(null);
-
-
-  var arc = d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius);
-
-  
-  var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-  
-  var slices = svg.selectAll("path")
-    .data(pie(ageCounts))
-    .enter()
-    .append("path")
-    .attr("d", arc)
-    .attr("fill", d => colorScale(d.data.age))
-    .attr("stroke", "#fff")
-    .attr("stroke-width", "2px");
-
-  
-  var labels = svg.selectAll("text")
-    .data(pie(ageCounts))
-    .enter()
-    .append("text")
-    .attr("transform", d => "translate(" + arc.centroid(d) + ")")
-    .attr("text-anchor", "middle")
-    .text(d => d.data.age + "age category (" + d.data.count + ")");
-});
+      d3.csv('titanic.csv').then(function(data) {
+        console.log("Age category: " + parseInt(data[1].Age / 10));
+        var grouped = d3.group(data, d => parseInt(d.Age / 10));
+        console.log(grouped);
+      
+        var counts = Array.from(grouped.values(), d => d.length);
+      
+        var pie = d3.pie()(counts);
+      
+        var width = 500;
+        var height = 500;
+        var radius = Math.min(width, height) / 2;
+      
+        var color = d3.scaleOrdinal()
+          .domain(counts)
+          .range(d3.schemeCategory10);
+      
+        var svg = d3.select("#piechart")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height);
+      
+        var g = svg.append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+      
+        var arc = d3.arc()
+          .innerRadius(0)
+          .outerRadius(radius);
+      
+        var arcs = g.selectAll("arc")
+          .data(pie)
+          .enter()
+          .append("g")
+          .attr("class", "arc");
+      
+        arcs.append("path")
+          .attr("fill", function(d) { return color(d.data); })
+          .attr("d", arc);
+      
+        arcs.append("text")
+          .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+          .attr("text-anchor", "middle")
+          .text(function(d) { return d.data; });
+      });
+      
 }
